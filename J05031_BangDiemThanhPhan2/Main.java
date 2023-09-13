f185fd5c36164b53ac5572cba1dd6be210953c6e
Hoang Dinh Hieu <hieuhoangatm@gmail.com>
2023-09-13 21:36:50 +0700

    Add files via upload

---
+++ J05031_BangDiemThanhPhan2/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePtit.J05031_BangDiemThanhPhan2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hoàng Đình Hiếu
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<SinhVien> arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            sc.nextLine();
+            arr.add(new SinhVien(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextDouble(),sc.nextDouble(),sc.nextDouble()));
+        }
+        Collections.sort(arr,new Comparator<SinhVien>(){
+            @Override
+            public int compare(SinhVien o1, SinhVien o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        int k =1;
+        for(SinhVien x : arr){
+            System.out.print(k+" ");
+            System.out.println(x);
+            k++;
+        }
+    }
+}
