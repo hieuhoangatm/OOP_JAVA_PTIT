fc544efe7045f82e3914c2cdfa0c67fa4ecc8ddd
Hoang Dinh Hieu <hieuhoangatm@gmail.com>
2023-09-21 23:48:14 +0700

    Add files via upload

---
+++ J07015_SoNguyenToTrongFileNhiPhan.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePtit;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Hoàng Đình Hiếu
+ */
+public class J07015_SoNguyenToTrongFileNhiPhan {
+    public static boolean ngTo(int x){
+        if(x < 2 ) return false;
+        for(int i=2;i<=Math.sqrt(x);i++){
+            if(x % i ==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        ObjectInputStream ob = new ObjectInputStream(new FileInputStream("SONGUYEN.in"));
+        ArrayList<Integer> arr = (ArrayList<Integer>) ob.readObject();
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(Integer x : arr){
+            if(ngTo(x)){
+                if(map.containsKey(x)){
+                    map.put(x, map.get(x)+1);
+                }
+                else{
+                    map.put(x,1);
+                }
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer key = entry.getKey();
+            Integer val = entry.getValue();
+            System.out.println(key+" "+val);
+        }
+    }
+}
